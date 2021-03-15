2eadc16343aa9ea7099784000800d755292b13b7
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2021-03-15 21:51:42 +0100

    modal gallery working done

--- Project/Versie_3/script.js
+++ Project/Versie_3/script.js
@@ -1,10 +1,10 @@
-const galleryImg = Array.from(document.querySelectorAll(".image"));
 const toggleMenu = document.querySelector(".toggle-menu");
 const fadeElements = document.querySelectorAll(".onload");
 window.addEventListener("scroll", animateImagesOnDisplay);
-const questions = document.querySelectorAll(".faq-container__aq-block ")
-
-
+const questions = document.querySelectorAll(".faq-container__aq-block ");
+const galleryImages = document.querySelectorAll(
+  ".gallery-container__img-container"
+);
 // animates navbar into view
 const navbar = document.querySelector("#navbar");
 navbar.classList.add("navbar-animate-onload");
@@ -52,39 +52,75 @@ window.addEventListener("load", () => {
 });
 
 // handles collapsable questions in faq.html
-questions.forEach(question=>{
-  question.addEventListener("click", ()=>{
-    const block = question.parentNode
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const block = question.parentNode;
     console.log(block);
-    question.classList.toggle("display-question")
-    const answer = question.childNodes[5]
-    const chevron = question.childNodes[1]
-    answer.classList.toggle("display-question")
-    chevron.classList.toggle("display-question")
-  })
-})
-
-const showModal = (image) => {
-  const modal = document.createElement("div");
-  document.body.appendChild(modal);
-  modal.innerHTML = `<img class="modal-image" src="${image}" alt="picture1">`;
-};
-
-// handles functionality of images in gallery.html
-galleryImg.forEach((image) => {
-  image.addEventListener("mouseenter", (e) => {
-    image.classList.add("image-hovered");
-  });
-  image.addEventListener("mouseleave", (e) => {
-    image.classList.remove("image-hovered");
-  });
-
-  image.addEventListener("click", (e) => {
-    const imageSrc = e.target.getAttribute("src");
-    showModal(imageSrc);
-    const modalImage = document.querySelector(".modal-image");
-    modalImage.addEventListener("click", (e) => {
-      e.target.parentNode.remove();
-    });
+    question.classList.toggle("display-question");
+    const answer = question.childNodes[5];
+    const chevron = question.childNodes[1];
+    answer.classList.toggle("display-question");
+    chevron.classList.toggle("display-question");
   });
 });
+
+const showModal = (e) => {
+  const modal = document.getElementById("modal-background");
+  modal.style.display = "flex";
+
+  const close = document.getElementById("close-modal");
+  const chevronRight = document.querySelector(".fa-chevron-right");
+  const chevronLeft = document.querySelector(".fa-chevron-left");
+  const modalImg = document.getElementById("modal-img");
+  const figcaptionEl = document.getElementById("modal-content__figcaption");
+  const images = Array.from(
+    document.querySelectorAll(".gallery-container__img")
+  );
+  let indexOfSelectedImage = images.findIndex(
+    (image) => image.src === e.target.src
+  );
+  let imgSrc = images[indexOfSelectedImage].src;
+  modalImg.src = imgSrc;
+  const imagesGalleryLength = images.length - 1;
+
+  const imgFigcaptionHtml =
+    images[indexOfSelectedImage].nextElementSibling.innerHTML;
+  figcaptionEl.innerHTML = imgFigcaptionHtml;
+
+  const closeModal = () => (modal.style.display = "none");
+  close.addEventListener("click", closeModal);
+  window.addEventListener("keydown", (e) => {
+    e.key === "Escape" && closeModal();
+  });
+
+  window.addEventListener("keydown", (e) => {
+    e.key === "ArrowRight" && getNextImage();
+    e.key === "ArrowLeft" && getPreviousImage();
+  });
+
+  const getNextImage = () => {
+    indexOfSelectedImage == imagesGalleryLength
+      ? (indexOfSelectedImage = 0)
+      : ++indexOfSelectedImage;
+    imgSrc = images[indexOfSelectedImage].src;
+    modalImg.src = imgSrc;
+    const imgFigcaptionHtml =
+      images[indexOfSelectedImage].nextElementSibling.innerHTML;
+    figcaptionEl.innerHTML = imgFigcaptionHtml;
+  };
+
+  const getPreviousImage = () => {
+    indexOfSelectedImage == 0
+      ? (indexOfSelectedImage = imagesGalleryLength)
+      : --indexOfSelectedImage;
+    imgSrc = images[indexOfSelectedImage].src;
+    modalImg.src = imgSrc;
+    const imgFigcaptionHtml =
+      images[indexOfSelectedImage].nextElementSibling.innerHTML;
+    figcaptionEl.innerHTML = imgFigcaptionHtml;
+  };
+
+  chevronRight.addEventListener("click", getNextImage);
+  chevronLeft.addEventListener("click", getPreviousImage);
+};
+galleryImages.forEach((image) => image.addEventListener("click", showModal));
